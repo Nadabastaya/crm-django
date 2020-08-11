ee6c95f42b50b37837e8f79027fe5cd0e0abac29
Nadabastaya <garcia.camilo@outlook.com>
2020-08-11 19:11:03 -0400

    add three views on accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+def costumer(request):
+    return HttpResponse('Costumer')
+
+def products(request):
+    return HttpResponse('Products')
