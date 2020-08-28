2228401972fd9c9ed8696dee2cce855f1ad854a2
Nadabastaya <garcia.camilo@outlook.com>
2020-08-28 17:51:21 -0400

    rendering data to templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+
+    orders = Order.objects.all()
+    customers = Costumer.objects.all()
+
+    total_orders = orders.count()
+    total_customers = customers.count()
+
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
+    context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'total_customers':total_customers, 'delivered':delivered, 'pending':pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def customer(request):
     return render(request, 'accounts/customer.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products':products})
